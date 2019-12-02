518169092a9427688a6ed057fc89eba849b32b3c
leetankhongs <55917426+leetankhongs@users.noreply.github.com>
2019-12-02 16:32:10 +0700

    Add login

--- routes/index.js
+++ routes/index.js
@@ -1,67 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 const productController = require("../Controller/ProductController");
-const User = require("../models/userModel");
-router.get('/', productController.home);
+const userController = require("../Controller/UserController");
 
+router.get('/', productController.home);
 router.get('/index', productController.home);
 router.get('/product_details/:id/:Brand', productController.detailProduct);
 router.get('/BrandProduct/:Brand', productController.brand);
 
-
-router.get('/register', function(req, res, next) {
-  res.render('Login/register.hbs', { title: 'Express' });
-});
-
-router.get('/login', function(req, res, next) {
-  res.render('Login/login.hbs', { title: 'Express' });
-});
-router.get('/forgetpass', function(req, res, next) {
-  res.render('Login/ForgetPass.hbs', { title: 'Express' });
-});
-  
-router.get('/User', function(req, res, next) {
-  res.render('Login/User.hbs', { title: 'Express' });
-});
-
-router.get('/SamSung', function(req, res, next) {
-  res.render('Phone/SamSung.hbs', { title: 'Express' });
-});
-router.get('/Iphone', function(req, res, next) {
-  res.render('Phone/Iphone.hbs', { title: 'Express' });
-});
-router.get('/Oppo', function(req, res, next) {
-  res.render('Phone/Oppo.hbs', { title: 'Express' });
-});
-router.get('/Nokia', function(req, res, next) {
-  res.render('Phone/Nokia.hbs', { title: 'Express' });
-});
-
-router.get('/Asus', function(req, res, next) {
-  products.find({Brand: 'Asus'})
-  .then(function(laptop)
-  {
-    res.render('Laptop/Asus.hbs',{laptops: laptop});
-  })
-});
-
-router.get('/Acer', function(req, res, next) {
-  res.render('Laptop/Acer.hbs', { title: 'Express' });
-});
-router.get('/Dell', function(req, res, next) {
-  res.render('Laptop/Dell.hbs', { title: 'Express' });
-});
-router.get('/HP', function(req, res, next) {
-  res.render('Laptop/HP.hbs', { title: 'Express' });
-});
-router.get('/Lenovo', function(req, res, next) {
-  res.render('Laptop/Lenovo.hbs', { title: 'Express' });
-});
-router.get('/MSI', function(req, res, next) {
-  res.render('Laptop/MSI.hbs', { title: 'Express' });
-});
 router.get('/cart', function(req, res, next) {
   res.render('Cart/Cart.hbs', { title: 'Express' });
 });
@@ -84,94 +33,6 @@ router.get('/VanChuyen', function(req, res, next) {
 });
 router.get('/GiaoHang', function(req, res, next) {
   res.render('Cart/States/GiaoHang.hbs', { title: 'Express' });
-});
-// router.get('/product_details', function(req, res, next) {
-//   res.render('Laptop/LaptopDetail.hbs', { title: 'Express' });
-// });
-router.post('/register', (req, res) =>
-{
-  const{Name,Email,Password,Password2,Address,Phone } =req.body;
-  let errors = [];
-
-  //check nhập đủ
-  if(!Name || !Email || !Password || !Password2 || !Address || !Phone)
-  {
-    errors.push({msg:'Hãy nhập tất cả các thông tin'});
-  }
-
-  //check password match
-
-  if(Password !== Password2)
-  {
-    errors.push({msg: "Mật khẩu không khớp"});
-  }
-
-  //check password length >=6 
-
-  if( Password.length < 6)
-  {
-    errors.push({msg:"Hãy nhập mật khẩu trên 6 kí tự"});
-  }
-
-  if(errors.length > 0 )
-  {
-    res.render('./Login/register',{
-      errors,
-      Name,
-      Email,
-      Password,
-      Password2,
-      Address,
-      Phone
-    })
-  }
-  else{
-    User.findOne({email:Email}).then(UserResult => {
-      if(UserResult)
-      {
-        errors.push({msg: "Email đã tồn tại"});
-        res.render('./Login/register',{
-         errors,
-         Name,
-         Email,
-         Password,
-         Password2,
-         Address,
-         Phone
-        });
-      }
-      else
-      {
-        //create new user
-        const newUser = new User ({
-          Name,
-          Email,
-          Password,
-          Address,
-          Phone
-        });
-  
-
-  //Hash password
-      bcrypt.genSalt(10,(err, salt)=>
-        bcrypt.hash(newUser.Password,salt,(err,hash)=>
-        {
-
-          if (err) throw err;
-          newUser.Password = hash;
-
-          //save
-          newUser.save().then(User => {
-            
-              req.flash('successs_msg','Đăng kí thành công. Đăng nhập ngay!!!');
-              res.redirect('/login');
-          }).catch(err => console.log(err));
-        }
-        ))
-          }
-        })
-    
-  } 
 });
 
 
