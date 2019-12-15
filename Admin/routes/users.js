399963e707ab586dc92e17840de4dd789dc4e3e0
sunnyhill93 <buido.huy99@gmail.com>
2019-12-15 20:40:52 +0700

    Thêm trang admin và chức năng xóa người dùng

---
+++ Admin/routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+let userController = require('../controllers/userController');
+
+/* GET users listing. */
+router.get('/', userController.loadUsers);
+
+router.post('/', userController.deleteUser);
+
+module.exports = router;
---
+++ Store/models/productModel.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+
+var productSchema = new mongoose.Schema({
+  Brand: {
+    type: String,
+    require: true
+  },
+  Name: {
+    type: String,
+    require: true
+  },
+  SimpleDetail: {
+    type: String,
+    require: true
+  },
+  Cost: {
+    type: Number,
+    require: true
+  },
+  Image: {
+    type: String,
+    require: true
+  },
+  TypeProduct: {
+    type: String,
+    require: true
+  },
+  Quantity: {
+    type: Number,
+    require: true
+  },
+  Description: {
+    type: String,
+    require: false
+  }
+}, { collection: 'products' });
+
+module.exports = mongoose.model('products', productSchema);
