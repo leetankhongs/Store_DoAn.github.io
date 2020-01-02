9a08d05b27ded73e5215006e5d67f2d8523d8c68
leetankhongs <55917426+leetankhongs@users.noreply.github.com>
2020-01-02 15:51:03 +0700

    convert giá tiền + chỉnh sửa lặt vặt

--- Store/app.js
+++ Store/app.js
@@ -97,5 +97,28 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+var hbs = require('hbs');
+hbs.registerHelper('_toStringMoney', function(integer) {
+  var temp = Number(integer) ;
+  var string ="";
+  var dem=0;
+
+  while(temp > 0)
+  {
+    string += (temp % 10).toString();
+    temp = parseInt (temp/10);
+    dem++;
+    if(dem===3 & temp!==0)
+    {
+      string +='.';
+      dem=0;
+    }
+  }
+  var arrayofchars = string.split("");
+  var strtoarray = arrayofchars.reverse();
+  var newstring = strtoarray.join("");
+  newstring +="đ";
+  return newstring;
+});
 
 module.exports = app;
