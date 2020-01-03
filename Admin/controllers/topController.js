2895f246095025d87a6dfd81e644bb78c419567e
sunnyhill93 <buido.huy99@gmail.com>
2020-01-03 19:30:58 +0700

    Hoàn thành top sản phẩm cho từng gian hàng

--- Admin/controllers/topController.js
+++ Admin/controllers/topController.js
@@ -10,8 +10,15 @@ random_rgba = () => {
 
 module.exports.topProducts = (req, res, next) => {
     (async() => {
+        let category = null;
+        if(req.query.Category) {
+            category = await categoryService.findCategory(req.query.Category);
+            if(!category) {
+                res.render('error.hbs', {message: "Error finding category"});
+            }
+        }
         const dictionary = {};
-        let desiredStates = [1,2];
+        let desiredStates = null;
         const allOrdersSatisfied = await orderService.getAllOrders(desiredStates);
         allOrdersSatisfied.forEach( order => {
             const cartItems = order.Cart.items;
@@ -19,8 +26,12 @@ module.exports.topProducts = (req, res, next) => {
             for(let x in cartItems)
             {
                 const item = cartItems[x];
-                if(dictionary[item.item._id]) dictionary[item.item._id] += item.qty * item.price;
-                else dictionary[item.item._id] = item.qty * item.price;
+                const regex = new RegExp(`\\b${item.item.TypeProduct}\\b`, "i");
+                if(category && !category.Type.match(regex)) {
+                    continue;
+                }
+                if(dictionary[item.item._id]) dictionary[item.item._id] += item.qty;
+                else dictionary[item.item._id] = item.qty;
             };
         });
         
@@ -34,25 +45,40 @@ module.exports.topProducts = (req, res, next) => {
         for(let i = 0; i < min; i++) {
             let max = i;
             for(let j = i + 1; j < items.length; j++){
-                if (items[j][1] > items[max][1]) max = j;
+                if (parseInt(items[j][1]) > parseInt(items[max][1])) {max = j;}
             } 
             [items[i], items[max]] = [items[max], items[i]];
         }
         items = items.slice(0, min);
         
+        const categoryDictionary = {};
         //Map each item with corresponding display
         for(let i = 0; i < items.length; i++){
             const productID = items[i][0];
             const product = await productService.findProductByID(productID);
-            const category = await categoryService.findCategory(product.TypeProduct);
+            let productCategory = category;
+            if(!category){
+                if(!categoryDictionary[product.TypeProduct]){
+                    productCategory = await categoryService.findCategory(product.TypeProduct);
+                    categoryDictionary[product.TypeProduct] = productCategory;
+                }
+                else{
+                    productCategory = categoryDictionary[product.TypeProduct];
+                }
+            }
             product.TOP = i + 1;
-            product.DisplayNameCategory = category.DisplayName;
+            product.DisplayNameCategory = productCategory.DisplayName;
             product.TotalSales = items[i][1];
             product.Color = random_rgba(); 
             items[i] = product;
         }
 
-        res.render('TOP/TopSP.hbs', {topItems: items, pollData: JSON.stringify(items)})
+        if(!category){
+            res.render('TOP/TopSP.hbs', {topItems: items, pollData: JSON.stringify(items)});
+            return;
+        }
+
+        res.render('TOP/TopGH.hbs', {topItems: items, pollData: JSON.stringify(items), category, pollCategory: JSON.stringify(category)});
         
     })();
 }
