f94c08dfe19b2054a8d013aea9e74198e71f1e7f
leetankhongs <55917426+leetankhongs@users.noreply.github.com>
2020-01-03 13:48:54 +0700

    Chỉnh sửa lịch sử đặt hàng

--- Store/routes/cart.js
+++ Store/routes/cart.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const CartController = require('../Controller/CartController');
 const UserController = require('../Controller/UserController');
-const Cart = require('../models/cart');
 router.get('/add-to-cart/:id', CartController.addToCart);
 router.post('/add-to-cart/:id', CartController.addToCart);
 
@@ -12,18 +11,6 @@ router.post('/pay', CartController.Pay);
 router.get('/reduce/:id', CartController.reduceNumberProduct);
 router.get('/increase/:id', CartController.increaseNumberProduct);
 router.get('/remove/:id', CartController.removeProduct);
-router.get('/status', CartController.statusProduct);
-router.post('/updateQty/:id',(req, res, next) => {
-    var productID = req.params.id;
-    const {qty} = req.body;
-    var cart = new Cart(req.session.cart?req.session.cart: {});
-    cart.updateQty(productID,qty);
-    req.session.cart = cart;
-    if(req.user)
-    {
-        req.user.Cart = cart;
-        req.user.save();
-    }
-    res.redirect('/cart/shopping-cart');
-} )
+
+router.post('/updateQty/:id',CartController.updateQty);
 module.exports = router;
